885da153263423c3e01e1d921300cfe5bf9783a7
Gennady Verdel <godrose@gmail.com>
2019-10-01 14:00:22 +0300

    chore: added multiple load tests

--- SomeAssembly.Facade.IntegrationTests/InitTests.cs
+++ SomeAssembly.Facade.IntegrationTests/InitTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
+using SomeAssembly.Contracts;
 using Xunit;
 
 namespace SomeAssembly.Facade.IntegrationTests
@@ -15,6 +16,26 @@ namespace SomeAssembly.Facade.IntegrationTests
             
             var consumer = serviceCollection.BuildServiceProvider().GetService(typeof(Consumer.Consumer));
             consumer.Should().NotBeNull();
+        }
+    }
+
+    public class MultipleLoadTests
+    {
+        [Fact]
+        public void ResolveDependencyTwice_AssembliesAreLoadedTwice_DependenciesAreTheSame()
+        {
+            var startup = new Startup();
+            var serviceCollection = new ServiceCollection();
+            startup.ConfigureServices(serviceCollection);
+
+            var dependency1 = serviceCollection.BuildServiceProvider().GetService(typeof(IDependency));            
+
+            startup = new Startup();
+            serviceCollection = new ServiceCollection();
+            startup.ConfigureServices(serviceCollection);
+            var dependency2 = serviceCollection.BuildServiceProvider().GetService(typeof(IDependency));
+
+            dependency1.GetHashCode().Should().Be(dependency2.GetHashCode());
         }
     }
 }
